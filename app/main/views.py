65a0d669a66547d2a3bb93602d4f3adbda8499fa
MEIYO\Meiyo <dengmingyao2016@gmail.com>
2017-11-23 00:00:35 +0800

    忘记了  　在资料页中显示博客文章

--- app/main/views.py
+++ app/main/views.py
@@ -17,7 +17,7 @@ def index():
 
     page = request.args.get('page', 1, type=int)
     pagination = Post.query.order_by(Post.timestamp.desc())\
-        .paginate(page, per_page=current_app.config('FLASKY_POSTS_PER_PAGE'), error_out=False)
+        .paginate(page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'], error_out=False)
     posts = pagination.items
     return render_template('index.html', form=form, posts=posts, pagination=pagination)
 
