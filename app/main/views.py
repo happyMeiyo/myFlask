c6dcfed9ef80f15b656267590aab286ba4848088
dengmingyao <dengmingyao@caibaopay.com>
2017-11-27 18:21:00 +0800

    博客文章编辑器

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request, current_app
+from flask import render_template, flash, redirect, url_for, request, current_app, abort
 from . import main
 from ..models import User, Role, Permission, Post
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
@@ -84,6 +84,22 @@ def post(id):
     return render_template('post.html', posts=[post])
 
 
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and (not current_user.can(Permission.ADMINISTER)):
+        abort(403)
+
+    form = PostForm()
+    if current_user.can(Permission.WRITE_ARTICLES) and form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        flash('The post has been updated.')
+        return redirect(url_for('.post', id=post.id))
+
+    form.body.data = post.body
+    return render_template('edit_post.html', form=form)
 
 
 
